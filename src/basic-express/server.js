a588b587ea7201c6b875568f68cd4c3cb2b50fa0
Mike Holzbach <maholzbach@gmail.com>
2019-07-04 10:19:18 -0400

    Added Pug

--- src/basic-express/server.js
+++ src/basic-express/server.js
@@ -1,15 +1,18 @@
 const express = require("express");
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "views", "404.html"));
