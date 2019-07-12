adc1e64fcad440bff8040d09d5e5abaf727633d4
Mike Holzbach <mikeholzbach@carfax.com>
2019-07-12 14:27:25 -0400

    Register new routes and flesh out a couple of new controllers

--- src/basic-express/controllers/products.js
+++ src/basic-express/controllers/products.js
@@ -1,7 +1,14 @@
 const Product = require("../models/product");
+const Cart = require("../models/cart");
 
+exports.getAdminProducts = (req, res) => {
+  res.render("admin/products", {
+    docTitle: "Admin Products",
+    path: "admin/products"
+  });
+};
 exports.getAddProduct = (req, res, next) => {
-  res.render("add-product", {
+  res.render("admin/add-product", {
     docTitle: "Add Product",
     path: "/admin/add-product"
   });
@@ -11,8 +18,22 @@ exports.postAddProduct = (req, res) => {
   product.save();
   res.redirect("/");
 };
+exports.getShop = (req, res) => {
+  Product.fetchAll(products => {
+    res.render("shop/product-list", { docTitle: "Shop", products, path: "/" });
+  });
+};
 exports.getProducts = (req, res) => {
   Product.fetchAll(products => {
-    res.render("shop", { docTitle: "Shop", products, path: "/" });
+    res.render("shop/product-list", {
+      docTitle: "Product List",
+      products,
+      path: "/product-list"
+    });
   });
+};
+exports.getCart = (req, res) => {
+  const cart = new Cart();
+  cart.listProducts();
+  res.render("shop/cart"), { docTitle: "Cart", path: "/shop/cart" };
 };
---
+++ src/basic-express/models/cart.js
@@ -0,0 +1,6 @@
+module.exports = class Cart {
+  constructor() {}
+  listProducts() {
+    console.log("CART CONTENTS.");
+  }
+};
