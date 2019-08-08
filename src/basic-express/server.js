f81b9fd350ee65ecb0fdee4ebaf4fa11b80b9fdb
Mike Holzbach <mikeholzbach@carfax.com>
2019-08-08 14:38:54 -0400

    add cart-item table

--- src/basic-express/server.js
+++ src/basic-express/server.js
@@ -7,6 +7,8 @@ const path = require("path");
 const sequelize = require("./util/database");
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 const app = express();
 
@@ -31,9 +33,13 @@ app.use(notFoundController.handle404);
 
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  //.sync({ force: true })
+  // .sync({ force: true })
   .sync()
   .then(result => {
     return User.findByPk(1);
