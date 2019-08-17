1a0b6c52444c7a480b6f373b0a89af6021d54a00
Mike Holzbach <maholzbach@gmail.com>
2019-08-17 12:42:01 -0400

    Adding and fetching users

---
+++ src/basic-express/models/user.js
@@ -0,0 +1,34 @@
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
+
+const ObjectId = mongodb.ObjectId;
+
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
+
+  save() {
+    const db = getDb();
+    return db
+      .collection("users")
+      .insertOne(this)
+      .then(result => console.log("RESULT LOG:", result))
+      .catch(err => console.log(err));
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new ObjectId(userId) })
+      .then(user => {
+        console.log(user);
+        return user;
+      })
+      .catch(err => console.log(err));
+  }
+}
+
+module.exports = User;
--- src/basic-express/server.js
+++ src/basic-express/server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const notFoundController = require("./controllers/404");
 const path = require("path");
 const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
 
 const app = express();
 
@@ -13,6 +14,15 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("5d582a1617dcfe22c8715666")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
