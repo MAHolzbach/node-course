3a6ce18ee81028fb6c1500ec823a3d280b6a866a
Mike Holzbach <maholzbach@gmail.com>
2019-08-26 19:47:21 -0400

    Create user model

--- src/basic-express/server.js
+++ src/basic-express/server.js
@@ -4,7 +4,7 @@ const shopRoutes = require("./routes/shop");
 const bodyParser = require("body-parser");
 const notFoundController = require("./controllers/404");
 const path = require("path");
-// const User = require("./models/user");
+const User = require("./models/user");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -15,14 +15,14 @@ app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById("5d582a1617dcfe22c8715666")
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("5d646ec8b592870b78b10b21")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -33,6 +33,16 @@ mongoose
     "mongodb+srv://mike:AyyptOYxjVNPnXEQ@nodecourse-7tzcj.mongodb.net/shop?retryWrites=true&w=majority"
   )
   .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: "Max",
+          email: "test@test.com",
+          cart: { items: [] }
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch(err => console.log(err));
